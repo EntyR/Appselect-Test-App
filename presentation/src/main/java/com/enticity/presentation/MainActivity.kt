17ece11952630b76b83fa5a41c53254c55ce1ff0
Andrei <ntr.reyzaxarov@gmail.com>
2022-06-23 16:21:26 +0300

    architecture set-up

--- presentation/src/main/java/com/enticity/presentation/MainActivity.kt
+++ presentation/src/main/java/com/enticity/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.enticity.appselect
+package com.enticity.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enticity.appselect.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
