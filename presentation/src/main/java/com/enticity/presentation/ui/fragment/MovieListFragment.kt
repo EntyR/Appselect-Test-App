247d8a715e95aedb643bdefcb4de80a67e52b963
Andrei <ntr.reyzaxarov@gmail.com>
2022-06-25 18:57:51 +0300

    movie list implementation

--- presentation/src/main/java/com/enticity/presentation/ui/fragment/MovieListFragment.kt
+++ presentation/src/main/java/com/enticity/presentation/ui/fragment/MovieListFragment.kt
@@ -1,17 +1,17 @@
 package com.enticity.presentation.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.enticity.appselect.R
+import androidx.recyclerview.widget.RecyclerView
 import com.enticity.appselect.databinding.FragmentMovieListBinding
-import com.enticity.domain.model.FilmModel
 import com.enticity.presentation.adapter.MovieAdapter
 import com.enticity.presentation.ui.view_model.MovieViewModel
+import com.enticity.presentation.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,21 +22,59 @@ class MovieListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMovieListBinding.inflate(inflater, container, false)
-        val movieAdapter = MovieAdapter()
+        val movieAdapter = MovieAdapter(requireContext())
         binding.rvMovieList.apply {
             adapter = movieAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
-        movieAdapter.submitList(
-            listOf(
-                FilmModel("","","")
-            )
-        )
-        viewvodel.receiveFilmList()
+
+        viewvodel.movieList.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    binding.btNextPage.visibility = View.GONE
+                    binding.btPrevPage.visibility = View.GONE
+                    movieAdapter.submitList(it.data)
+                }
+                Status.ERROR -> Unit
+                Status.LOADING -> Unit
+            }
+        }
+
+        binding.btNextPage.setOnClickListener {
+            viewvodel.nextPage()
+        }
+        binding.btPrevPage.setOnClickListener {
+            viewvodel.previousPage()
+        }
+
+        viewvodel.page.observe(viewLifecycleOwner) { _ ->
+            updateFilmList()
+        }
+
+        binding.rvMovieList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    binding.btNextPage.visibility = View.VISIBLE
+                    if (viewvodel.page.value!! != 0)
+                        binding.btPrevPage.visibility = View.VISIBLE
+                } else {
+                    binding.btNextPage.visibility = View.GONE
+                    binding.btPrevPage.visibility = View.GONE
+                }
+
+            }
+
+        })
 
         return binding.root
+    }
+
+    private fun updateFilmList() {
+        viewvodel.receiveFilmList(viewvodel.page.value!!.toInt() * 20)
+
     }
 
     companion object {
