5598b83e24808e9f4b5a36209200a94980d44468
Andrei <ntr.reyzaxarov@gmail.com>
2022-06-25 11:31:01 +0300

    adding hilt

--- presentation/src/main/java/com/enticity/presentation/MainActivity.kt
+++ presentation/src/main/java/com/enticity/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.enticity.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.enticity.appselect.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ presentation/src/main/java/com/enticity/presentation/application/AppSelectApplication.kt
@@ -0,0 +1,7 @@
+package com.enticity.presentation.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppSelectApplication: Application()
